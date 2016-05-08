373a0948bbd66758ce6d863faa4f19ebaee75390
Vanessa Sant'Anna <pair+vanessa.santanna@gmail.com>
2016-05-08 18:44:52 -0400

    Add host to photo url

--- app/uploaders/cat_photo_uploader.rb
+++ app/uploaders/cat_photo_uploader.rb
@@ -8,6 +8,7 @@ class CatPhotoUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   storage :file
+  asset_host  ActionController::Base.asset_host
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
